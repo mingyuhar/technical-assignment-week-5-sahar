6a110a2d1c0a4210e3454773aab702c8b6bdc45e
mingyuhar <105527013+mingyuhar@users.noreply.github.com>
2022-07-14 12:03:48 +0700

    Update pir_sensor.py

--- pir_sensor.py
+++ pir_sensor.py
@@ -1,9 +1,11 @@
-from gpiozero import MotionSensor
+#Script sensor.py dan sebuah fungsi untuk mengambil data dari sensor tersebut
 
-pir = MotionSensor(4)
+from gpiozero import MotionSensor   # mengambil package import Motion Sensor
 
-pir.wait_for_motion()
-print("Motion Detected!")
+pir = MotionSensor(4)   # PIR Sensor yang terhubung dengan GPIO 4
+
+pir.wait_for_motion()     # Sistem akan menunggu gerakan yang datang
+print("Motion Detected!")   #Apabila ada gerakan output Motion Detected
 
 while True:
-    print("ada maling")
+    print("ada maling")     #Apabila hasil True, keluaran menghasilkan ada Maling
